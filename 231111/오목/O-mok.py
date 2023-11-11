ff7cf54cb0db4f197e1d197b49c8d212d79f33c6
JS KANG <49180081+jisoo920@users.noreply.github.com>
2023-11-12 01:36:31 +0900

    Title: 오목, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231111/오목/O-mok.py
+++ 231111/오목/O-mok.py
@@ -2,36 +2,40 @@ MAX = 19
 
 girds = [ list(map(int, input().split())) for _ in range(MAX)]
 
+black = [1, 1, 1, 1, 1]
+white = [2, 2, 2, 2, 2]
+
+
 # 가로
 def check_hor(r, c): 
     #print("가로:", girds[r][c:c+5])
-    num_sum = sum(girds[r][c:c+5])
-
-    if num_sum == 10 or num_sum == 5:
+    
+    if girds[r][c:c+5] == black or girds[r][c:c+5] == white:
         return True
     else:
         return False
 
 # 세로
 def check_ver(r, c):
-    num_sum = 0
+    num_list = []
     for n_r in range(r, r+5):
-        #print(girds[n_r][c], end=' ')
-        num_sum += girds[n_r][c]
-
-    if num_sum == 10 or num_sum == 5:
+        num_list.append(girds[n_r][c])
+    
+    #print(num_list)
+    
+    if num_list == black or num_list == white:
         return True
     else:
         return False
 
+
 # 대각선
 def check_dia(r, c):    
-    num_sum = 0
+    num_list = []
     for n in range(5):
-        #print(girds[r+n][c+n], end=' ')
-        num_sum += girds[r+n][c+n]
+        num_list.append(girds[r+n][c+n])
 
-    if num_sum == 10 or num_sum == 5:
+    if num_list == black or num_list == white:
         return True
     else:
         return False
@@ -68,17 +72,18 @@ for row in range(MAX-4):
         
         
 
-#print("n_row, n_col:", n_row, n_col)
+#print("n_row, n_col, how:", n_row, n_col, how)
 
 # 이긴 돌
 print(girds[n_row][n_col])
 
-# 가로
-if how == 'hor':     
-    print(n_row+1, n_col+3)
-# 세로
-elif how == 'ver' :
-    print(n_row+3, n_col+1)
-# 대각선
-elif how == 'dia' :
-    print(n_row+3, n_col+3)
+if girds[n_row][n_col] != 0:
+    # 가로
+    if how == 'hor':     
+        print(n_row+1, n_col+3)
+    # 세로
+    elif how == 'ver' :
+        print(n_row+3, n_col+1)
+    # 대각선
+    elif how == 'dia' :
+        print(n_row+3, n_col+3)
