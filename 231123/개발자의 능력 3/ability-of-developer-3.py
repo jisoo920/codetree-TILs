b7cc3d407e78fef1289f203cf9610ed1357c77d5
JS KANG <49180081+jisoo920@users.noreply.github.com>
2023-11-23 07:50:59 +0900

    Title: 개발자의 능력 3, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231123/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,23 @@
+developers = list(map(int, input().split()))
+
+sum_de = sum(developers)
+def get_diff(i, j, k):
+    sum_1= developers[i] + developers[j] + developers[k]
+    sum_2 = sum_de - sum_1
+    return abs(sum_1-sum_2)
+
+
+
+
+len_de = len(developers)
+
+
+answer = 99999
+
+for i in range(len_de):
+    for j in range(i+1, len_de):
+        for k in range(j+1, len_de):
+            
+            answer = min(answer, get_diff(i, j, k))
+
+print(answer)
