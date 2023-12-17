3dd968934986c663b86f61482e1a39591b1c590e
JS KANG <49180081+jisoo920@users.noreply.github.com>
2023-12-18 08:05:10 +0900

    Title: 야바위, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 231218/야바위/ya-rock.py
+++ 231218/야바위/ya-rock.py
@@ -4,21 +4,15 @@ directions = [list(map(int, input().split())) for _ in range(n)]
 answer = 0
 
 
-for s in range(3):    
-    stone = [0, 0, 0]
+for s in range(1, 4):    
+    stone = [0]*4
     stone[s] = 1
     
     score = 0
 
-    for d in directions:
-        a = d[0]-1
-        b = d[1]-1
-        c = d[2]-1
-
-        tmp = stone[a]
-        stone[a] = stone[b]
-        stone[b] = tmp
-
+    for a, b, c in directions:
+        stone[a], stone[b] = stone[b], stone[a]
+        
         if stone[c] == 1:
             score += 1
     
