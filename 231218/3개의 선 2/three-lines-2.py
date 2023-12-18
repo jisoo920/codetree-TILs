83d7a3ef370a0df17754503a797e8e8db5654221
JS KANG <49180081+jisoo920@users.noreply.github.com>
2023-12-18 21:13:28 +0900

    Title: 3개의 선 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231218/3개의 선 2/three-lines-2.py
+++ 231218/3개의 선 2/three-lines-2.py
@@ -5,17 +5,21 @@ possible = 0
 
 for hor in range(10):
     for ver in range(10):
-        checked = [0] * n
+        checked = [0] * n 
 
-        for i in range(len(coord)):
+        for i in range(n):
             if coord[i][0] == ver or coord[i][1] == hor :
                 checked[i] = 1
-            
+                #print(checked)
     
-    if sum(checked) == n:
-        possible += 1
+        if sum(checked) == n:
+            possible += 1
 
-if possible <= 3:
+        #print("hor, ver, possible", hor, ver, possible)
+
+#print("possible", possible)
+
+if possible == 3:
     print(1)
 else:
     print(0)
