0797755d43eae0db6fd6cf8ece3ecc172c977a30
JS KANG <49180081+jisoo920@users.noreply.github.com>
2024-01-04 22:25:26 +0900

    Title: 숫자들의 최대 차, Time: 206ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240104/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -0,0 +1,22 @@
+n, k = map(int, input().split())
+nums = [int(input()) for _ in range(n)]
+
+MAX = max(nums)
+
+def count_len(min_n, max_n):
+    cnt = 0
+
+    for num in nums:
+        if num >= min_n and num <= max_n:
+            cnt += 1
+    
+    return cnt
+
+ans = 0
+
+for i in range(MAX+1):
+    max_num = i + k
+
+    ans = max(ans, count_len(i, max_num))
+
+print(ans)
