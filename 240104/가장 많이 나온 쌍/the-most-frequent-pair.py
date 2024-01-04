2e28a6dcf8eb8c4505bed5ebc6a033dedeff10be
JS KANG <49180081+jisoo920@users.noreply.github.com>
2024-01-04 22:17:06 +0900

    Title: 가장 많이 나온 쌍, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240104/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 240104/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -4,15 +4,13 @@ pairs = [list(map(int, input().split())) for _ in range(m)]
 ans = 0
 
 for i in range(1, n+1):
-    for j in range(1, n+1):
+    for j in range(i+1, n+1):
         cnt = 0
        
         for a, b, in pairs:
-            if (a == i) and (b == j):
+            if (i, j) in [(a, b), (b, a)]:
                 cnt += 1
-            elif (a == j) and (b == i):
-                cnt += 1
-        
+                
         ans = max(ans, cnt)
 
 print(ans)
