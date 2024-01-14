d24524d3825c306d791e08e76e6f10562cdcc281
JS KANG <49180081+jisoo920@users.noreply.github.com>
2024-01-15 08:28:34 +0900

    Title: 이상한 폭탄 3, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240115/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,27 @@
+n, k = map(int, input().split())
+bumbs = [int(input()) for _ in range(n)]
+#(폭탄정수, 횟수)
+answer = list()
+
+unique_b = list(set(bumbs))
+
+for unique in unique_b:
+    idx = 0
+    cnt = 0 # 터진 횟수
+    for i in range(1, n):
+        if bumbs[i] == unique: 
+            if abs(idx-i) <= k:
+                cnt += 1
+            idx = i
+
+    if cnt > 0:
+        answer.append((unique, cnt))
+
+    #print(answer)
+
+if len(answer) == 0:
+    print(0)
+else:
+    answer.sort(key = lambda x:-x[1])
+    print(answer[0][0])
+    # 터진 횟수기준 정렬, 폭탄 번호 출력
