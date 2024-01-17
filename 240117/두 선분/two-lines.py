c2651a58019144e656a63ed73612629bc118be62
JS KANG <49180081+jisoo920@users.noreply.github.com>
2024-01-17 22:23:06 +0900

    Title: 두 선분, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240117/두 선분/two-lines.py
+++ 240117/두 선분/two-lines.py
@@ -1,15 +1,9 @@
 x1, x2, x3, x4 = map(int, input().split())
 
-intersect = False
+intersect = True
 
-if x1 <= x3 <= x2:
-    intersect = True
-elif x1 <= x4 <= x2:
-    intersect = True
-elif x3 <= x1 <= x4:
-    intersect = True
-elif x3 <= x2 <= x4:
-    intersect = True
+if x2 < x3 or x4 < x1:
+    intersect = False
 
 if intersect:
     print("intersecting")
