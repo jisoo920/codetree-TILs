3d59da242697f1106426fc2b5a4a49b2dc31453c
JS KANG <49180081+jisoo920@users.noreply.github.com>
2024-02-05 23:41:07 +0900

    Title: 최대 최소간의 차, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240205/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -0,0 +1,35 @@
+import sys
+
+n, k = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+answer = sys.maxsize
+
+MAX = max(numbers)
+MIN = min(numbers)
+
+def get_cost(min_num, max_num):
+    cost = 0
+    for num in numbers:
+        # 범위안에 들어오면 pass
+        if num >= min_num and num <= max_num: 
+            continue
+        # 최소값보다 작으면 최소값으로
+        elif num < min_num:
+            cost += abs(num - min_num)
+        # 최대값보다 크면 최대값으로
+        elif num > max_num:
+            cost += abs(num - max_num)
+        
+    #print("cost: ", cost)
+    return cost
+    
+        
+for i in range(MIN, MAX+1):
+    max_num = i+k
+    #print("min_num, max_num: ",i,  max_num)
+    answer = min(answer, get_cost(i, max_num))
+
+#    print(answer)
+
+print(answer)
