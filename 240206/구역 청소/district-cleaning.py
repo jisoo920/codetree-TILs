15f77418a0aa1f82d9146143c18236bc130a4f52
JS KANG <49180081+jisoo920@users.noreply.github.com>
2024-02-06 23:37:48 +0900

    Title: 구역 청소, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240206/구역 청소/district-cleaning.py
@@ -0,0 +1,19 @@
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+
+# 겹치지 않는 경우 
+if b < c or d < a:
+    answer = (b-a) + (d-c)
+
+# 일부가 겹치는 경우
+elif c < b < d or a < d < b:
+    answer = max(b, d) - min(a, c)
+
+# 포함되어 겹치는 경우
+elif c < a and b < d:
+    answer = d - c
+elif a < c and d < b:
+    answer = b - a
+
+
+print(answer)
