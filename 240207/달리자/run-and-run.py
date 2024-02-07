3dd6cf3c392c53356d3ef0a99c2b0f55cbc22777
JS KANG <49180081+jisoo920@users.noreply.github.com>
2024-02-07 22:25:48 +0900

    Title: 달리자, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240207/달리자/run-and-run.py
@@ -0,0 +1,17 @@
+n = int(input())
+a_arr = list(map(int, input().split()))
+b_arr = list(map(int, input().split()))
+
+answer = 0
+
+for idx in range(n-1):
+    diff = a_arr[idx] - b_arr[idx]
+    answer += diff
+
+    a_arr[idx] -= diff
+    a_arr[idx+1] += diff
+
+    #print("a_arr: ", a_arr)
+    #print("idx, diff, answer: ", idx, diff, answer)
+
+print(answer)
