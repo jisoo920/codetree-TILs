d23d9a44a941ef72a90322012d6a4e2006bd3460
JS KANG <49180081+jisoo920@users.noreply.github.com>
2024-02-09 22:39:19 +0900

    Title: 코딩톡, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240209/코딩톡/coding-talk.py
+++ 240209/코딩톡/coding-talk.py
@@ -1,9 +1,16 @@
 # 사람 수, 메시지 개수, p번째 메시지
 n, m, p = map(int, input().split())
+alpha = [chr(c) for c in range(ord('A'),ord('Z')+1)][:n]
 
-messages = [list(input().split()) for _ in range(m)]
 
-print(messages)
+reader = list()
+for idx in range(1, m+1):
+    programer, message = input().split()
 
-for i in range(26):
-    print(A+i)
+    if idx >= p:
+        reader.append(programer)
+
+unreader = list(set(alpha) - set(reader))
+
+for r in unreader:
+    print(r, end = ' ')
